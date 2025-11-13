85b39dbe47979036c4aa9af817a8a54571c07d04
Sara Rodríguez Pardo <rodriguezps@easdalcoi.es>
2025-11-13 09:22:53 +0100

    Add files via upload

---
+++ js/dom_elements.js
@@ -0,0 +1,36 @@
+let canvas;
+let input_txt;
+let slider_value;
+let value=0;
+let value_r=255;
+let value_g=255;
+let value_b=255;
+let w_r=20;
+let h_r=20;
+
+function setup() {
+  canvas=createCanvas(400, 400);
+  input_txt=createInput('read 🔮me');
+  slider_value=createSlider(1,100,10);
+  slider_value.position(200,20);
+}
+
+function draw() {
+  background(value);
+  input_txt.position(w_r,h_r);
+  fill(value_r,value_g,value_b);
+  textSize(slider_value.value());
+  text(input_txt.value(),20,height/2);
+}
+  
+function deviceShaken() {
+  value = value + 5;
+  if (value > 255) {
+    value = 0;
+  }
+  w_r=random(width);
+  h_r=random(height);
+  value_r=random(255);
+  value_g=random(255);
+  value_b=random(255);
+}
